e1ec1978c4d690b1423336800ff36bcde8e74762
gabriel <gabriel.g.robin@airbus.com>
2024-12-19 10:49:19 +0100

    starting multi backend

--- src/xplor/model.py
+++ src/xplor/model.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, TypedDict, Unpack, overload
 
 import polars as pl
 
@@ -11,22 +11,41 @@ if TYPE_CHECKING:
     from xplor.gurobi.model import XplorGurobi
 
 
-class Xplor[M: "gp.Model | mathopt.Model"]:
+class GurobiKwargs(TypedDict, total=False):
+    """Optional kwargs for Gurobi."""
+
+    deterministic: bool
+    auto_update: bool
+
+
+class MathOptKwargs(TypedDict, total=False):
+    """Optional kwargs for MathOpt."""
+
+
+class Xplor:
     """Xplor base class to wrap your OR model."""
 
     _backend: "XplorGurobi"
 
-    def __init__(self, model: M, *, deterministic: bool = False, auto_update: bool = False) -> None:
+    @overload
+    def __init__(self, model: "gp.Model", **backend_kwargs: Unpack[GurobiKwargs]) -> None: ...
+
+    @overload
+    def __init__(self, model: "mathopt.Model", **backend_kwargs: Unpack[MathOptKwargs]) -> None: ...
+
+    def __init__(
+        self,
+        model: "gp.Model | mathopt.Model",
+        **backend_kwargs: Any,
+    ) -> None:
         """Initialize an Xplor instance.
 
         Parameters
         ----------
         model : gp.Model or math_opt.Model
             The Model to wrap
-        deterministic : bool, default=False
-            Whether to ensure deterministic behavior when creating variables.
-        auto_update : bool, default=False
-            Whether to automatically update the model after adding variables or constraints.
+        backend_kwargs: MathOptKwargs | GurobiKwargs
+            Solver specific configuration
 
         Notes
         -----
@@ -36,20 +55,18 @@ class Xplor[M: "gp.Model | mathopt.Model"]:
 
         """
         self.model = model
-        self._set_backend()
+        self._set_backend(backend_kwargs)
 
-        self.deterministic = deterministic
-        self.auto_update = auto_update
         self.constrs: dict[str, pl.DataFrame] = {}
         self.vars: dict[str, pl.DataFrame] = {}
 
-    def _set_backend(self) -> None:
+    def _set_backend(self, backend_kwargs: Any) -> None:
         if (gp_model := _dependencies.get_gurobipy_model_type()) is not None and isinstance(
             self.model, gp_model
         ):
             from xplor.gurobi.model import XplorGurobi
 
-            self._backend = XplorGurobi(self.model)
+            self._backend = XplorGurobi(self.model, **backend_kwargs)
 
     def add_constrs(
         self,
